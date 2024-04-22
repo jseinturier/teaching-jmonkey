f615e940dd9a2c58426d5be6e976c0e368ff9922
Julien SEINTURIER <seinturier@gmail.com>
2024-04-22 11:05:24 +0200

    Working on animation

--- jmonkey-tutorials/src/main/java/fr/utln/jmonkey/model/GLBAnimatedModel.java
+++ jmonkey-tutorials/src/main/java/fr/utln/jmonkey/model/GLBAnimatedModel.java
@@ -1,12 +1,6 @@
 package fr.utln.jmonkey.model;
 
-import com.jme3.anim.AnimClip;
-import com.jme3.anim.AnimComposer;
 import com.jme3.anim.SkinningControl;
-import com.jme3.anim.tween.Tweens;
-import com.jme3.anim.tween.action.Action;
-import com.jme3.anim.tween.action.BaseAction;
-import com.jme3.anim.tween.action.LinearBlendSpace;
 import com.jme3.app.SimpleApplication;
 import com.jme3.asset.plugins.FileLocator;
 import com.jme3.environment.EnvironmentCamera;
@@ -17,6 +11,8 @@ import com.jme3.light.DirectionalLight;
 import com.jme3.light.LightProbe;
 import com.jme3.light.SpotLight;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Spatial;
 
@@ -25,53 +21,36 @@ public class GLBAnimatedModel extends SimpleApplication {
 	private int frame = 0;
 	
 	private SkinningControl sc;
-    private static Action currentAction;
 	
 	@Override
 	public void simpleInitApp() {
-		DirectionalLight sun = new DirectionalLight();
-        sun.setDirection(new Vector3f(0,0,0).normalizeLocal());
-        sun.setColor(ColorRGBA.White);
-        rootNode.addLight(sun);
-
-        AmbientLight al = new AmbientLight();
-        al.setColor(ColorRGBA.White.mult(1.3f));
-        rootNode.addLight(al);
-		
-        SpotLight sl = new SpotLight(new Vector3f(0.0f, 0.0f, 0.0f), Vector3f.UNIT_Y);
-        sl.setColor(ColorRGBA.White.mult(1.3f));
-        rootNode.addLight(sl);
-        
-        // Load the GLB model
+      
+        // Load the GLB model from the data folder
         assetManager.registerLocator("data", FileLocator.class);
-        Spatial animatedModel = assetManager.loadModel("HoverRocket.glb");
+        Spatial animatedModel = assetManager.loadModel("model/HoverRocket.glb");
 
+        // Place the model correctly according the JMonkey referential
+        Quaternion rotation = new Quaternion();
+        rotation.fromAngles(-FastMath.HALF_PI, 0.0f, 0.0f);
+        animatedModel.setLocalRotation(rotation);
+  
         rootNode.attachChild(animatedModel);
 
         // PBR - Environment camera initialization
+        // This initialization is necessary if the model contain PBR material
+        // see: https://wiki.jmonkeyengine.org/docs/3.4/tutorials/how-to/articles/pbr/pbr_part1.html
+        //
+        // PBR handling need also the definition of a light probe within update method.
         final EnvironmentCamera envCam = new EnvironmentCamera(256, new Vector3f(0.0f, 0.0f, 0.0f));
         stateManager.attach(envCam);
         
+        // Animation - Initializing animation
         sc = animatedModel.getControl(SkinningControl.class);
 
-
-/*
-        animComposer.actionBlended("Attack", new LinearBlendSpace(0f, 0.5f), "Dodge");
-        for (AnimClip animClip : animComposer.getAnimClips()) {
-            Action action = animComposer.action(animClip.getName());
-            if(!"stand".equals(animClip.getName())) {
-                action = new BaseAction(Tweens.sequence(action, Tweens.callMethod(this, "backToStand", animComposer)));
-            }
-            animComposer.addAction(animClip.getName(), action);
-        }
-        currentAction = animComposer.setCurrentAction("stand"); // Walk, pull, Dodge, stand, push
-*/      
-        
-        
-        
-        
+        // Configure a chase camera for a better experience
         flyCam.setEnabled(false);
         ChaseCamera chaseCam = new ChaseCamera(cam, animatedModel, inputManager);
+        
         
         this.getViewPort().setBackgroundColor(ColorRGBA.LightGray);
 	}
@@ -82,15 +61,15 @@ public class GLBAnimatedModel extends SimpleApplication {
 		frame++;
 		
 		// PBR - LightProbe initialization
-		// Rendering need to be effective before adding LightProbe
+		// Rendering need to be effective before adding LightProbe, 
+		// this is the reason the rendering of the frame 2 is required 
+		// before initializing the light probe. 
 		if (frame == 2) {
 			final LightProbe probe = LightProbeFactory.makeProbe(stateManager.getState(EnvironmentCamera.class), rootNode);
 	        
 	        probe.getArea().setRadius(100);
 	        rootNode.addLight(probe);
 		}
-
-
 	}
 	
 	/**
